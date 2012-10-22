b70d6fc420f262406c6efaf40217f0c38d273549
Jonathan Pepin <jypepin@gmail.com>
2012-10-22 12:00:49 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitchapp::Application.config.secret_token = '27fc222122df03764c76d22ded1d2e4c94dac1a43806843f532bc57cfb2d514af65e9b0e840c89553f3e0e9799c0dd77e9ddd5923e8d70c7b255bb660a25f66f'
