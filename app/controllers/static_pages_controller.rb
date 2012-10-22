56f760164113d58790ce170dc9f95c7b7c44623e
Jonathan Pepin <jypepin@gmail.com>
2012-10-22 14:27:54 -0700

    it works

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,20 @@
 class StaticPagesController < ApplicationController
   def home
-      url = "https://api.twitch.tv/kraken/games/top"
-       get = RestClient.get(url)
-       json = JSON.parse(get)
-       @every_top_games = json["top"]
+    @top_games = get_top_games
+    @top_streams = get_top_streams
   end
 
+  def get_top_games
+    url = "https://api.twitch.tv/kraken/games/top"
+    rest = RestClient.get(url)
+    json = JSON.parse(rest)
+    json["top"]
+  end
+
+  def get_top_streams
+    url = "https://api.twitch.tv/kraken/streams"
+    rest = RestClient.get(url)
+    json = JSON.parse(rest)
+    json["streams"]
+  end
 end
