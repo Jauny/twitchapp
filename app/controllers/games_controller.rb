2a115d877c3417f56aa93e3ebd4b81bdf7c90483
Jonathan Pepin <jypepin@gmail.com>
2012-11-10 00:17:47 -0800

    fix stream sort

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,7 +7,9 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
 
     @game_streams = []
-    Stream.where(:game => @game.name).each do |stream|
+    Stream.where(:game => @game.name).sort do |a,b|
+      b.viewers <=> a.viewers
+    end.each do |stream|
       @game_streams << stream
       break if @game_streams.length == 10
     end
